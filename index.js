1f502ecf7bc690aca32c2b8c79544322185e8620
ShamimaNasrin <shamima.nasrion007@gmail.com>
2022-10-26 20:59:32 +0600

    courses api

--- index.js
+++ index.js
@@ -5,9 +5,17 @@ const port = process.env.PORT || 5000;
 
 app.use(cors());
 
+const courses = require('./data/courses.json');
+const courseInfo = require('./data/courseInfo.json');
+
 app.get('/', (req, res) => {
     res.send('Courses API Running');
 });
+
+//create api for courses Name
+app.get('/courses', (req, res) => {
+    res.send(courses);
+})
 
 app.listen(port, () => {
     console.log('Courses Server running on port', port);
